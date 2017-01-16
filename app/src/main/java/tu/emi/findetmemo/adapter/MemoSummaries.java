43fbf9797f6ed13dca47be91350224213e6a4092
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2017-01-16 17:47:10 +0100

    implement swipe to delete memo

--- app/src/main/java/tu/emi/findetmemo/adapter/MemoSummaries.java
+++ app/src/main/java/tu/emi/findetmemo/adapter/MemoSummaries.java
@@ -1,6 +1,10 @@
 package tu.emi.findetmemo.adapter;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +15,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
+import tu.emi.findetmemo.R;
 import tu.emi.findetmemo.activity.MainActivity;
 import tu.emi.findetmemo.data.Memo;
 import tu.emi.findetmemo.data.MemoRepository;
@@ -22,6 +27,7 @@ public class MemoSummaries extends RecyclerView.Adapter<BaseViewHolder> {
     private final MemoRepository memos;
 
     private MemoRepository.Observer observer = null;
+    private final ItemTouchHelper touchHelper;
     private int attachedCount = 0;
 
     private final ArrayList<Memo> sortedMemos;
@@ -29,10 +35,55 @@ public class MemoSummaries extends RecyclerView.Adapter<BaseViewHolder> {
 
     private final MainActivity parent;
 
+    private class SwipeToDelete extends ItemTouchHelper.Callback {
+        private final VectorDrawableCompat iconDelete;
+
+        private SwipeToDelete() {
+            super();
+            iconDelete = VectorDrawableCompat.create(parent.getResources(), R.drawable.ic_delete_white_24dp, parent.getTheme());
+        }
+
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            return makeMovementFlags(0, ItemTouchHelper.LEFT);
+        }
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+            memos.remove(sortedMemos.get(viewHolder.getAdapterPosition()).uuid);
+        }
+
+        @Override
+        public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            Paint p = new Paint();
+            View v = viewHolder.itemView;
+            p.setColor(0xFFFF0000);
+            c.drawRect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom(), p);
+
+            double padding = v.getHeight() * 0.1;
+            int left = (int)(v.getRight() - v.getHeight() + padding);
+            int right = (int)(v.getRight() - padding);
+            int top = (int)(v.getTop() + padding);
+            int bottom = (int)(v.getBottom() - padding);
+
+            iconDelete.setBounds(left, top, right, bottom);
+            iconDelete.draw(c);
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+
+    }
+
     public MemoSummaries(MemoRepository memos, MainActivity parent) {
         this.comparator = new Memo.TitleComparator();
         this.memos = memos;
         this.parent = parent;
+        this.touchHelper = new ItemTouchHelper(new SwipeToDelete());
 
         this.sortedMemos = new ArrayList<>(memos.all());
         Collections.sort(sortedMemos, comparator);
@@ -46,6 +97,9 @@ public class MemoSummaries extends RecyclerView.Adapter<BaseViewHolder> {
             attachedCount++;
             return;
         }
+
+
+        touchHelper.attachToRecyclerView(recyclerView);
 
         observer = memos.registerObserver(new MemoRepository.Observer() {
             @Override
@@ -107,6 +161,7 @@ public class MemoSummaries extends RecyclerView.Adapter<BaseViewHolder> {
             return;
         }
 
+        touchHelper.attachToRecyclerView(null);
         memos.unregisterObserver(observer);
         attachedCount--;
         if (attachedCount != 0) throw new RuntimeException("shouldn't happen");
@@ -118,7 +173,7 @@ public class MemoSummaries extends RecyclerView.Adapter<BaseViewHolder> {
     public int getItemViewType(int position) {
         ViewTemplate template = sortedMemos.get(position).summaryViewTemplate();
         viewTypes.put(template.layoutRes, template);
-        return template.layoutRes;
+            return template.layoutRes;
     }
 
     @Override
